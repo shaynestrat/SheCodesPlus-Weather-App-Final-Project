6569c0ac33d84b2cb2e9f0575fd785b058646b76
shaynestrat <69431959+shaynestrat@users.noreply.github.com>
2020-11-14 12:25:46 -0500

    Changed api url to metric (from units)

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function displayWeatherCondition(response) {
 
 function searchCity(city) {
   let apiKey = "5694f43f4dbaf07d0e041cc20cf76b31";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=units`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -51,7 +51,7 @@ function handleSubmit(event) {
 function seachLocation(position) {
   let apiKey = "5694f43f4dbaf07d0e041cc20cf76b31";
 
-  let apiUrl = `"https://api.openweathermap.org/data/2.5/weather"?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units}=${units}`;
+  let apiUrl = `"https://api.openweathermap.org/data/2.5/weather"?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(getPosition);
 }
